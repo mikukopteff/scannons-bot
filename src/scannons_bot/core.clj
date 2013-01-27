f6d5832407aacb81b34677b2f5e2d97006503730
Mikael Kopteff <miku.kopteff@gmail.com>
2013-01-27 21:14:02 +0200

    Add http library on some basic functions.

--- src/scannons_bot/core.clj
+++ src/scannons_bot/core.clj
@@ -1,11 +1,19 @@
 (ns scannons-bot.core
   (:require [clj-http.client :as client]))
 
-(def move (client/get "http://localhost:8080/move/luke/s/200"))
+(defn f-slash [param] (str param \/))
 
-(defn init
-  []
-  (println move))
+(defn move [dir, amount] 
+  (client/get (str "http://localhost:8080/move/luke/" 
+                   ( f-slash dir) 
+                   (f-slash amount))))
+
+(defn pau [] (println "hello-s"))
+
+(defn status [] (client/get "http://localhost:8080/status")) 
+
+(defn init []
+  (get (status) :body))
 
 (defn -main
   [& args]

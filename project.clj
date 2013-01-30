d8e4a59bd3ace0bbad40d4facc3fd8c9cd41178e
Mikael Kopteff <miku.kopteff@gmail.com>
2013-01-30 22:29:47 +0200

    Make some generic code using first class functions.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/mikukopteff/scannons"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"], [clj-http "0.6.3"]]
+  :dependencies [[org.clojure/clojure "1.4.0"], [clj-http "0.6.3"], [org.clojure/data.json "0.2.1"]]
   :main scannons-bot.core)
--- src/scannons_bot/core.clj
+++ src/scannons_bot/core.clj
@@ -1,19 +1,21 @@
 (ns scannons-bot.core
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client], [clojure.data.json :as json]))
 
 (defn f-slash [param] (str param \/))
 
 (defn move [dir, amount] 
   (client/get (str "http://localhost:8080/move/luke/" 
-                   ( f-slash dir) 
+                   (f-slash dir) 
                    (f-slash amount))))
 
-(defn pau [] (println "hello-s"))
+(defn status [] 
+  (client/get "http://localhost:8080/status")) 
 
-(defn status [] (client/get "http://localhost:8080/status")) 
+(defn parse-body [fn-request] 
+  (json/read-str (get (fn-request) :body)))
 
 (defn init []
-  (get (status) :body))
+  (println (parse-body status)))
 
 (defn -main
   [& args]
